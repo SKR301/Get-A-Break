4bd9f65bd294ca75d6282d65db92c63c27d10568
Saurav KumaR <chunnu.bihat@gmail.com>
2021-09-09 16:36:44 +0530

    added base GUI for windows #5

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import tkinter
 import time
 
-WorkTime = 5		# enter the amount of time you wanna work
-SnoozeTime = 3		# enter snooze time
-
+WorkTime = 1200								# enter the amount of time you wanna work
+SnoozeTime = 300							# enter snooze time
+msg = "HEY, Take a break nerd!"				# your text
 
 def showAlert(root):
 	root.title("Get-A-Break")
-	root.geometry("500x500")
+	root.geometry("250x100")
 
-	Start = tkinter.Button(root, text ="Start", command = lambda: startTimer(WorkTime, root))
-	Start.pack()
-	Snooze = tkinter.Button(root, text ="Snooze", command = lambda: startTimer(SnoozeTime, root))
-	Snooze.pack()
-	End = tkinter.Button(root, text ="End", command = lambda: quit())
-	End.pack()
+	var = tkinter.StringVar()
+	var.set(msg)
+	label = tkinter.Message( root, textvariable=var, width = 150)
+	label.place(x=10, y=5)
+
+	OK = tkinter.Button(root, text = "OK", width = 7, height = 1, command = lambda: startTimer(WorkTime, root))						#restart
+	OK.place(x=100,y=50)
+	Snooze = tkinter.Button(root, text = "Snooze", width = 7, height = 1, command = lambda: startTimer(SnoozeTime, root))			# snooze
+	Snooze.place(x=170,y=50)
 
 	root.mainloop()
 
 def startTimer(timerTime, root):
 	root.destroy()
-	print("Timer Started:")
-	print(timerTime)
+	# print("Timer Started:")
+	# print(timerTime)
 	time.sleep(timerTime)
 	
 	root = tkinter.Tk()
